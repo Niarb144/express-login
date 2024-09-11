854bf4bfe8623c124089e4e6089b3a774a937223
Brian Teddy <teddybrian543@gmail.com>
2024-09-11 09:43:13 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,80 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+const db = new pg.Client({
+  user : "postgres",
+  host : "localhost",
+  database : "secrets",
+  password : "Ted123",
+  port : 5432
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  let uName = req.body.username;
+  let uPassword = req.body.password;
+
+  try{
+    const checkResult = await db.query(
+      "SELECT * FROM users WHERE user_email = $1" ,[uName]
+    );
+  
+    if (checkResult.rows.length > 0){
+      res.send("Email already exists. Try Loging in");
+    }
+    else{
+      console.log(`Username is ${uName} and Password is ${uPassword}`);
+      const result = await db.query(
+        "INSERT INTO users (user_email, user_password) VALUES ($1, $2)" , [uName, uPassword]
+      );
+      res.render("secrets.ejs");
+    }
+  }
+ catch(err){
+    console.log(err);
+ }
+  
+});
+
+app.post("/login", async (req, res) => {
+  let uName = req.body.username;
+  let uPassword = req.body.password;
+
+  const checkUser = await db.query(
+    "SELECT * FROM users WHERE user_email = $1", [uName]
+  );
+
+  // if (checkUser){
+  //   const userPass = checkUser.rows.;
+  //   if (userPass == uPassword){
+  //     res.render("secrets.ejs");
+  //     console.log(`Username is ${uName} and Password is ${uPassword}`);
+  //   }
+  //   else{
+  //     res.send("Wrong password. Try again");
+  //   }
+  // }
+  
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
